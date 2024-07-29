05f7382e9351a08148e88a416c7e352b87118f57
magbor <magbor@rm.dk>
2024-07-29 15:32:39 +0200

    add ts rollup plugin

--- openapi/config.ts
+++ openapi/config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import type {  NitroOptions, Nitro } from 'nitropack'
 import fileRouterPlugin from './rollup-file-router-plugin'
+import sucrase from '@rollup/plugin-sucrase'
 
 export default function openAPINitroOptions(nitro: Nitro): NitroOptions {
 	return <NitroOptions>{
@@ -23,7 +24,18 @@ export default function openAPINitroOptions(nitro: Nitro): NitroOptions {
 		inlineDynamicImports: true, // important to make sure routes in /routes are not lazily loaded (which will make addValidatedRoute not run before route is first hit)
 	
 		rollupConfig: {
+			// input: './index.ts',
+			input: fileURLToPath(new URL('index.ts', import.meta.url)),
+			output: {
+				dir: './dist',
+				format: 'cjs',
+				exports: 'named'
+			},
 			plugins: [
+				sucrase({
+					exclude: ['node_modules/**'],
+					transforms: ['typescript'],
+				}),
 				fileRouterPlugin(nitro)
 			],
 		},
